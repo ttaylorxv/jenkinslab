e2cab93dc49b7a8969bbfd21be34b3493a4990ac
Tonye Taylor-Harry <ttaylor@crossvale.com>
2018-02-20 14:04:20 -0600

    update node modules to include chai, supertest, and express

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', (req, res) => res.send('Hello Mini World!'))
+app.get('/', (req, res) => res.send('Hello To the Jenkins World!'))
 
 module.exports = app.listen(8080, () => console.log('Example app listening on port 8080!'))
